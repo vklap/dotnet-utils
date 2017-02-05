c9dd2ce4599ebe2dc50ee6930c9798aec3fe7c46
Victor Klapholz <VictorK@dm.cx>
2017-02-05 12:51:16 +0200

    Init commit with DateTimeProvider

---
+++ Utils.Tests/DateTimeProviderTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using Utils.Abstract;
+using Utils.Concrete;
+
+namespace Utils.Tests
+{
+    [TestClass]
+    public class DateTimeProviderTests
+    {
+        private IDateTimeProvider CreateProvider()
+        {
+            return new DateTimeProvider();
+        }
+
+        [TestMethod]
+        public void CurrentDateTime()
+        {
+            // Arrange 
+            var provider = CreateProvider();
+
+            // Act
+            var result = provider.CurrentDateTime;
+
+            // Assert
+            result.ShouldBeGreaterThanOrEqualTo(DateTime.UtcNow.AddSeconds(-1));
+            result.ShouldBeLessThan(DateTime.UtcNow.AddSeconds(1));
+        }
+    }
+}
---
+++ Utils/Abstract/IDateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils.Abstract
+{
+    public interface IDateTimeProvider
+    {
+        DateTime CurrentDateTime { get; }
+    }
+}
